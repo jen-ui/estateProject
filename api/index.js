40643855dda0536ce3bb83a833b44b36669d81ff
Jenisha Khulal <jenishakhulal@gmail.com>
2023-12-13 14:05:13 +0545

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import { log } from "console";
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("server started at port no.3000");
+mongoose.connect(
+  process.env.MONGO_URL
+).then(() => {
+    console.log('Database connected');
+}).catch((e) => {
+    console.log(e);
+})
+
+app.listen(5000, () => {
+    console.log("server started at port no. 4000");
 })
